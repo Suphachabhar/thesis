2a669515fb92448e5e81f5b9d1581448c00b3c0b
kerubintin <tkifung@yahoo.com.hk>
2020-10-21 17:25:04 +1100

    password fixed

--- user.sql
+++ user.sql
@@ -2,8 +2,8 @@
 -- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Oct 21, 2020 at 07:53 AM
+-- Host: 127.0.0.1
+-- Generation Time: Oct 21, 2020 at 08:24 AM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -40,8 +40,9 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `email`, `user_type`, `password`) VALUES
-(1, 'tingting', 'tingting@gmail.com', 1, '123'),
-(2, 'tt', 'tt@gmail.com', 0, '9adcb29710e807607b683f62e555c22dc5659713');
+(1, 'tingting', 'tingting@gmail.com', 1, '202cb962ac59075b964b07152d234b70'),
+(2, 'tt', 'tt@gmail.com', 0, '9adcb29710e807607b683f62e555c22dc5659713'),
+(4, 'hacking', 'test@gmail.com', 0, 'e1a08da1dd23a36f9c1cefb5d3e44e34');
 
 --
 -- Indexes for dumped tables
@@ -61,7 +62,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
