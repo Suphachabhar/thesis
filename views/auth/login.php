27772351e7f1b659a98a0f656f26fcfd1270ce3c
Suphachahar <z5176892@ad.unsw.edu.au>
2020-10-26 17:21:03 +1100

    change the login page

---
+++ views/auth/login.php
@@ -0,0 +1,53 @@
+<?php include('server.php') ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registration system PHP and MySQL</title>
+	
+	<link href="style.css" rel="stylesheet">
+</head>
+<body>
+	<img class="wave" src="img/wave.png">
+	<div class="container">
+	
+		<div class="login-content">
+			<form method="post" action="login.php">
+
+			<?php echo display_error(); ?>
+				<img src="img/avatar.svg">
+				<h2 class="title">Login Form</h2>
+
+				<div class="input-div one">
+					<div class="i">
+						<i class="fas fa-user"></i>
+					</div>
+					<div class="div">
+           		   		<h5>Username</h5>
+           		   		<input type="text" class="input" name="username">
+           		   </div>
+				</div>
+
+				<div class="input-div pass">
+           		   <div class="i"> 
+           		    	<i class="fas fa-lock"></i>
+           		   </div>
+           		   <div class="div">
+           		    	<h5>Password</h5>
+           		    	<input type="password" class="input" name="password">
+            	   </div>
+				</div>
+				
+				<input type="submit" class="btn" value="Login" name="login_btn">
+
+				<p>
+					Not yet a member? <a href="registration.php">Sign up</a>
+				</p>
+
+				</form>
+		</div>
+
+	</div>
+	<script type="text/javascript" src="style.js"></script>
+</body>
+</html>
