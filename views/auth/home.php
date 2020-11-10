e03e392859623217a4841d9ca178bdc0ebb2ea3e
Suphachahar <z5176892@ad.unsw.edu.au>
2020-11-10 12:30:06 +1100

    change

--- views/auth/home.php
+++ views/auth/home.php
@@ -1,23 +1,35 @@
 <?php 
 include('server.php');
 
-
 if (isset($_GET['logout'])) {
 	session_destroy();
 	unset($_SESSION['user']);
 	header("location: auth/login.php");
 }
 ?>
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Home</title>
-	<link href="style.css" rel="stylesheet">
+<meta charset="UTF-8">
+
+	<title>Course</title>
+	<link href="home.css" rel="stylesheet">
+	
 	
 </head>
 <body>
+	<nav>
+      <div id="logo-img">
+          <a href="home.php">
+              <img src="img/unsw_0.png" href="home.php">
+		  </a>
+      </div>
+      
+  	</nav>
+
 	<div class="header">
-		<h2>Admin - Home Page</h2>
+		
 	</div>
 	<div class="content">
 		<!-- notification message -->
